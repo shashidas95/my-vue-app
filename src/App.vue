5ff547d8394323855dfdf38e0b0ba33ebfdb47fe
shashidas95 <shashidas95@gmail.com>
2023-07-01 11:23:42 +0600

    ref $ reactive

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>{{ course.name }}</h1>
+  <h1>course by: {{ course.mentor }}</h1>
+  <h1>platform  {{ course.platform }}</h1>
+  <button @click="count++">+</button>
+  <h1>{{ count }}</h1>
+  <button @click="count--">-</button>
+</template>
+
+<script setup>
+import { ref, reactive } from 'vue'
+let count = ref(0)
+count.value = 10
+let course = reactive({
+  name: 'vue js masterclass',
+  mentor: 'shashi',
+  platform: 'online',
+})
+course.name = 'vue js 3 masterclass'
+</script>
